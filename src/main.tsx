d2a7599e85573cc0520338288f563891ca02d290
IN_RD <ivanrud23@yandex.ru>
2025-11-30 22:01:47 +0300

    1. удалена папка storybook 2. Миграция на SCSS — установка, миграция переменных и классов 3. Добавлены Json

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './shared/fonts/index.css';
-import './index.css';
-import App from './App';
+import './shared/styles/index.scss';
+import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
