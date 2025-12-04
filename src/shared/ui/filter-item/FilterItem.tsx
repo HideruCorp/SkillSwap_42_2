1a62c64457cb4f34b920cac46ae8b3c4b4a76401
Aziev Damir <hideru.a-dam@yandex.com>
2025-12-04 23:21:37 +0500

    feat(ui): add filter item component
    - FilterItem.tsx - создан компонент FilterItem с отображением типа фильтра, значения и кнопкой удаления
    - type.ts - добавлены типы FilterType, SearchTypeValue, GenderValue и интерфейс FilterItemProps
    - filter-item.module.scss - добавлены стили для компонента фильтра
    - index.ts - добавлен экспорт FilterItem

---
+++ src/shared/ui/filter-item/FilterItem.tsx
@@ -0,0 +1,36 @@
+import CrossIcon from '@shared/assets/img/cross.svg?react';
+
+import type { FilterItemProps, FilterType } from './type';
+import styles from './filter-item.module.scss';
+
+function getDisplayText(type: FilterType, value: string): string {
+  switch (type) {
+    case 'searchType':
+      return value;
+    case 'category':
+      return value;
+    case 'gender':
+      return `Пол: ${value}`;
+    case 'city':
+      return value;
+    case 'name':
+      return `Название: *${value}*`;
+    default:
+      return value;
+  }
+}
+
+function FilterItem({ type, value, onClick }: FilterItemProps) {
+  const displayText = getDisplayText(type, value);
+
+  return (
+    <button className={styles['filter-item']} onClick={onClick} type="button">
+      <span>{displayText}</span>
+      <span className={styles.icon}>
+        <CrossIcon />
+      </span>
+    </button>
+  );
+}
+
+export default FilterItem;
