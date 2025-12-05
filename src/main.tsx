13a4ffc4b1af851c4bfb16ac89d16830861b4bb1
Aziev Damir <hideru.a-dam@yandex.com>
2025-12-05 12:38:51 +0500

    feat(store): integrate redux store
    - main.tsx - добавлен Provider с Redux store
    - src/services/slices/filtersSlice/index.ts - единая точка импорта filterSlice
    - .eslintrc.json - добавлено правило разрешающее модификацию состояния в редюсерах

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './shared/styles/index.scss';
+import { Provider } from 'react-redux';
+import store from './services/store';
+import '@shared/styles/index.scss';
 import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
