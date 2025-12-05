8e52af48e9b90a89a59579cdbc53921bbf01a843
Anton Panyushkin <p_spear@mail.ru>
2025-12-05 15:53:58 +0300

    feat: add ErrorPageContent

---
+++ src/widgets/ErrorPageContent/ErrorPageContent.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '@shared/ui/button/Button';
+import type { ErrorPageContentProps } from './type';
+import styles from './ErrorPageContent.module.scss';
+
+export default function ErrorPageContent({ image, title, description }: ErrorPageContentProps) {
+  const navigate = useNavigate();
+  const handleRedirectHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={image} alt={title} />
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.message}>{description}</p>
+      <div className={styles.buttons}>
+        <Button
+          title="Сообщить об ошибке"
+          type="default"
+          onClick={() => alert('Отчёт об ошибке отправлен')}
+        />
+        <Button title="На главную" type="primary" onClick={handleRedirectHome} />
+      </div>
+    </div>
+  );
+}
