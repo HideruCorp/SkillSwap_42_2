1accb3e7cb4cea8ac382b9f54881185f593c4438
molka333 <cinov.nek@gmail.com>
2025-12-06 22:16:54 +0300

    реализовал компонент инпут

---
+++ src/shared/ui/Input/InputUI.stories.tsx
@@ -0,0 +1,70 @@
+// InputUI.stories.tsx
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import InputUI from './InputUI';
+
+const meta = {
+  title: 'UI/Input',
+  component: InputUI,
+} satisfies Meta<typeof InputUI>;
+
+export default meta;
+
+export const Basic: StoryObj<typeof InputUI> = {
+  render: function Render() {
+    const [value, setValue] = useState('');
+    return (
+      <InputUI
+        value={value}
+        onChange={setValue}
+        placeholder="Введите что-нибудь..."
+      />
+    );
+  },
+};
+
+export const WithError: StoryObj<typeof InputUI> = {
+  render: function Render() {
+    const [value, setValue] = useState('');
+    const error = value.length > 0 && value.length < 3 ? 'Минимум 3 символа' : undefined;
+    
+    return (
+      <InputUI
+        value={value}
+        onChange={setValue}
+        placeholder="Введите минимум 3 символа"
+        error={error}
+      />
+    );
+  },
+};
+
+export const Password: StoryObj<typeof InputUI> = {
+  render: function Render() {
+    const [value, setValue] = useState('');
+    return (
+      <InputUI
+        value={value}
+        onChange={setValue}
+        placeholder="Введите пароль"
+        type="password"
+        message='Надежный'
+      />
+    );
+  },
+};
+
+
+export const Edit: StoryObj<typeof InputUI> = {
+  render: function Render() {
+    const [value, setValue] = useState('Мария');
+    return (
+      <InputUI
+        value={value}
+        onChange={setValue}
+        placeholder="Введите имя"
+        type="change"
+      />
+    );
+  },
+};
