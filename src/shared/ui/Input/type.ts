5c071b3a12755ab7e993366614056e7f362eff2b
AntonZhuravskiy <petropak2014@yandex.ru>
2025-12-12 18:44:54 +0300

    feat: профиксил конфликты

--- src/shared/ui/Input/type.ts
+++ src/shared/ui/Input/type.ts
@@ -2,9 +2,10 @@ export interface InputProps {
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
-  type?: 'text' | 'change' | 'password';
+  type?: 'text' | 'change' | 'password' | 'email';
   error?: string;
   message?: string;
+  label?: string;
   disabled?: boolean;
 }
 
